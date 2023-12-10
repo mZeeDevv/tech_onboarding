afe80a7ac88b2eeaedf404def85bcc1f9977760a
mZeeDev <hamzeekhattak56@gmail.com>
2023-12-10 10:37:16 +0500

    Fixed bug

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/profile.jpg";
+import image from "../images/socials/profile.jpg";
 
 const imageAltText = "Hamza Khattak's Profile Picture";
 
